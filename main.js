3526c7074389b0bd3cbfc8dd34c2151722c7a7cd
Smoqee <kaan.private@outlook.de>
2022-08-01 10:46:04 +0200

    Fix layout AND Add border style

--- main.js
+++ main.js
@@ -1,39 +1,31 @@
-const container = document.querySelector('.container')
-let totalWidth = container.offsetWidth-2
+const field = document.querySelector('#field')
+
+
+function createField(amount) {
+    field.innerHTML = ""
+    for (let index = 0; index < amount*amount; index++) {
+        const cell = document.createElement('div')
+        cell.classList.add('item')
+        field.appendChild(cell)
+        cell.addEventListener('mouseover', changeColor)
+    }
+    field.style.cssText = `grid-template: repeat(${amount}, ${100/amount}%) / repeat(${amount}, ${100/amount}%);`
+    
+}
+
+createField(16)
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+    let length = prompt('grid length: ')
+    length > 0 && length < 101 ?  createField(length): alert("größer 0 und kleiner gleich 100!")
+   
+
+})
+
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
 
 function changeColor(e) {
     e.target.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
 }
-
-button = document.querySelector('button')
-
-button.addEventListener('click', () => {
-    let length = prompt('grid length: ')
-    createNewLayout(length) 
-})
-
-
-function createNewLayout(length) {
-
-    if(length > 100 || length < 1) {
-        alert('between 1 and 100')
-        return
-    }
-
-    container.innerHTML = ""
-
-    for(let i = 0; i<length*length; i++) {
-        const square = document.createElement('div')
-        square.style.width = `${totalWidth/length}px`
-        square.style.height = `${totalWidth/length}px`
-        square.classList.add('sqr')
-    
-        square.addEventListener('mouseover', changeColor)
-        container.appendChild(square)    
-    }
-    
-}
-
-
-createNewLayout(16)
